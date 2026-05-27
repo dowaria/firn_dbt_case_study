14aea9bc144c27d35f7cae0156bf26ae1cd33caa
gourav.dowaria@gmail.com <gourav.dowaria@gmail.com>
2026-05-27 06:57:20 +0000

    Added new filed to ev_roam_charging_stations_transformation

--- models/silver/ev_roam_charging_stations_transformed.sql
+++ models/silver/ev_roam_charging_stations_transformed.sql
@@ -26,7 +26,7 @@ with
             ev_roam_charging_stations_transformed,
             lateral split_to_table(connectors_list, '},{')
     ),
-   connector_split as (
+    connector_split as (
         select
             station_id,
             station_name,
@@ -36,6 +36,7 @@ with
             latitude,
             longitude,
             has_charging_cost,
+            case when has_charging_cost = 'TRUE' then 'Paid' else 'Free' end as charing_cost,
             is_24_hours,
             global_id,
             first_operational_date,
@@ -43,9 +44,13 @@ with
             trim(split_part(connector_details, ',', 1)) as current_type,
             trim(split_part(connector_details, ',', 2)) as power_kw,
             trim(split_part(connector_details, ',', 3)) as connector_type,
-            trim(replace(split_part(connector_details, ',', 4), 'Status:', '')) as connector_status,
-            trim(replace(split_part(connector_details, ',', 5), 'Count:', '')) as connector_count
-        from 
-            connector_list_exploded
-   )
-   select * from connector_split
+            trim(
+                replace(split_part(connector_details, ',', 4), 'Status:', '')
+            ) as connector_status,
+            trim(
+                replace(split_part(connector_details, ',', 5), 'Count:', '')
+            ) as connector_count
+        from connector_list_exploded
+    )
+select *
+from connector_split
